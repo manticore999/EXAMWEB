47abeea35f292c762a6c449186c6444df000ccbc
Farah <farah.cherif@insat.ucar.tn>
2024-05-28 20:53:26 -0400

    authentification

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
 use App\Repository\PersonneRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +16,13 @@ class LoginController extends AbstractController
 {
     private $personneRepository;
     private $passwordHasher;
+    private $entityManager;
 
-    public function __construct(PersonneRepository $personneRepository, UserPasswordHasherInterface $passwordHasher)
+    public function __construct(PersonneRepository $personneRepository, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager)
     {
         $this->personneRepository = $personneRepository;
         $this->passwordHasher = $passwordHasher;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -49,5 +53,46 @@ class LoginController extends AbstractController
         }
 
         return $this->render('login/index.html.twig');
+    }
+
+    /**
+     * @Route("/signup", name="signup")
+     */
+    public function signup(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            $username = $request->request->get('username');
+            $email = $request->request->get('email');
+            $password = $request->request->get('password');
+            $confirmPassword = $request->request->get('confirm_password');
+
+            if ($username && $email && $password && $confirmPassword) {
+                if ($password !== $confirmPassword) {
+                    $this->addFlash('danger', 'Passwords do not match.');
+                } else {
+                    $existingUser = $this->personneRepository->findOneByUsername($username) ??
+                        $this->personneRepository->findOneByEmail($email);
+
+                    if ($existingUser) {
+                        $this->addFlash('danger', 'Username or email already taken.');
+                    } else {
+                        $user = new Personne();
+                        $user->setUsername($username);
+                        $user->setEmail($email);
+                        $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+
+                        $this->entityManager->persist($user);
+                        $this->entityManager->flush();
+
+                        $this->addFlash('success', 'Account created successfully.');
+                        return $this->redirectToRoute('login');
+                    }
+                }
+            } else {
+                $this->addFlash('danger', 'Please complete all fields.');
+            }
+        }
+
+        return $this->render('login/signup.html.twig');
     }
 }
